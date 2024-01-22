4c9f5ff6d142bc3956addcb7ab3cd8133a08ce79
MarcoAurélioO <marcoaureliofernandes.mike@gmail.com>
2024-01-22 12:46:34 -0300

    curso e python
    Fiz a aula 6 , 7, 8 e um exercício de variável em Python e a correção do exercício

---
+++ CURSO_PYTHON/exercicio.py
@@ -0,0 +1,13 @@
+nome = 'Marco'
+sobrenome = 'Aurélio'
+idade = int(22)
+ano_nascimento = int(2024 - idade)
+maior_de_idade = bool(idade >= 18)
+altura_metros = float (1.72)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print( 'Altura em metros:', altura_metros)
