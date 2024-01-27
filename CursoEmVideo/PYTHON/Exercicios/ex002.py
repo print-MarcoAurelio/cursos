5c5ab79388365084538a15021aeb3b9cd87a454e
MarcoAurélioO <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-01-27 04:31:08 -0300

    python

--- CursoEmVideo/PYTHON/Exercicios/ex002.py
+++ CursoEmVideo/PYTHON/Exercicios/ex002.py
@@ -1,2 +1,4 @@
-nome = 
-print('É um grande prazer em conher', nome)
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas - vindas.
+
+nome = input('Qual é o seu nome: ')
+print('É um prazer te conher, {}!'.format(nome))
---
+++ CursoEmVideo/PYTHON/Exercicios/ex003.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia dois número e mostra a soma entre eles
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+s = n1 + n2
+
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
