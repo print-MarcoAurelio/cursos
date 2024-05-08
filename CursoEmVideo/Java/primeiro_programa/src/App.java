f8d4018500574888a0dc577d0c7f8854c6a92b88
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-05-08 19:22:35 -0300

    curso em video
    psvm e sout

--- CursoEmVideo/Java/primeiro_programa/src/App.java
+++ CursoEmVideo/Java/primeiro_programa/src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Olá, Mundo!");
     }
 }
+
+/*psvm*/
+/*sout*/
