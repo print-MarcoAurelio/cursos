3227c9fcb35553c49b212f7c99320358ab88b09a
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-05-15 18:14:14 -0300

    sanai
    ex 18

---
+++ Cisco/Fundamentos_do_Python/Atividade1/ex18.py
@@ -0,0 +1,8 @@
+# Exercício 19 Desenvolva um software que calcule o volume de uma lata de refrigerante imprimir o resultado.
+
+import math
+raio = float(input("Digite o raio da lata de refrigerente (em centínetros): "))
+altura = float(input("Digite a altura da lata de refrigerante (em centímetros)"))
+volume = math.pi * (raio ** 2) * altura
+
+print(f"O volume da lata de refrigerante é: {volume:.2f} cm³")
