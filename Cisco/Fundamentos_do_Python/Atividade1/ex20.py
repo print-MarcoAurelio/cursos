0821a346a88d45531f78d479bfb5428c4a47a02c
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-06-12 13:37:37 -0300

    ex20

---
+++ Cisco/Fundamentos_do_Python/Atividade1/ex20.py
@@ -0,0 +1,35 @@
+# Exercício 21 Desenvolver um programa que leia um valor numérico inteiro entre os valores 1 e
+# 12 e apresentar por extenso o nome do mês correspondente ao valor entrado. Caso sejam
+# fornecidos valores menores que 1 e maiores que 12, o programa deve apresentar a mensagem
+# "Valor inválido".
+
+# numero_mes = int(input("Digite um número entre 1 e 12 para o mês desejado: "))
+
+# if numero_mes == 1:
+#     nome_mes = "Janeiro"
+# elif numero_mes == 2:
+#     nome_mes = "Fevereiro"
+# elif numero_mes == 3:
+#     nome_mes = "Março"
+# elif numero_mes == 4:
+#     nome_mes = "Abril"
+# elif numero_mes == 5:
+#     nome_mes = "Maio"
+# elif numero_mes == 6:
+#     nome_mes = "Junho"
+# elif numero_mes == 7:
+#     nome_mes = "Julho"
+# elif numero_mes == 8:
+#     nome_mes = "Agosto"
+# elif numero_mes == 9:
+#     nome_mes = "Setembro"
+# elif numero_mes == 10:
+#     nome_mes = "Outubro"
+# elif numero_mes == 11:
+#     nome_mes = "Novembro"
+# elif numero_mes == 12:
+#     nome_mes = "Dezembro"
+# else:
+#     nome_mes = "Valor inválido"
+
+# print(nome_mes)
