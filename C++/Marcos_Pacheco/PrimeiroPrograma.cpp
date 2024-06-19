47c4ab8d86b274e455743297898bf542e2164563
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-06-19 20:57:57 -0300

    Create PrimeiroPrograma.cpp

---
+++ C++/Marcos_Pacheco/PrimeiroPrograma.cpp
@@ -0,0 +1,12 @@
+// PrimeiroPrograma.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "pch.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Primeiro Programa C++" << std::endl;
+	return 0;
+}
