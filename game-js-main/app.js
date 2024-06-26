ae1dd7b132969a36922b475a0848b291892b4dc4
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-06-26 18:13:30 -0300

    Update app.js

--- game-js-main/app.js
+++ game-js-main/app.js
@@ -1,3 +1,9 @@
 alert("Boas vindaas ao jogo do número secreto!");
 let numeroSecreto = 29;
-prompt("Escolha um número entre 1 e 30");
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (numeroSecreto == chate) {
+    console.log("Isso ai! Você descobriu o número secreto (5)");
+}else{
+    
+}
