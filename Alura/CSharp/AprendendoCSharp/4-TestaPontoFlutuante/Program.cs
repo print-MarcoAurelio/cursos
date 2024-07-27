abb1c5440266d945d9d07f7a48263ddfeeb9bb95
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-07-27 18:48:02 -0300

    .

---
+++ Alura/CSharp/AprendendoCSharp/4-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("4 - Teste Ponto Flutuante");
+
+        double salario;
+        salario = 1250.70;
+
+        Console.WriteLine("Meu salario é " + salario);
+
+        int divisao = 7 / 5;
+        Console.WriteLine(divisao);
+
+        Console.WriteLine("Aperte o enter para sair");
+        Console.ReadLine();
+    }
+}
