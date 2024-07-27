b2f9a2f30747e0e9839c387a52896d2fe775db3d
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-07-27 18:47:16 -0300

    .

---
+++ Alura/CSharp/AprendendoCSharp/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 300.15;
+        Console.WriteLine(salario);
+
+        //O int é um tipo de variável de 64 bits
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        //O long é um tipo de variável de 64 bits
+        long x = 2000000000000000000;
+        Console.WriteLine(x);
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Aperta enter para fechar...");
+        Console.ReadLine();
+    }
+}
