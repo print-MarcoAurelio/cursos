b25a6c6da7347c34230ed339073b81bb1c18d2e9
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-07-27 18:46:34 -0300

    .

---
+++ Alura/CSharp/AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
