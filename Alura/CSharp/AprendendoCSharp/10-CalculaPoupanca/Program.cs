3f46327f897c63b119ff196789fb856a01e29fae
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-08-01 22:04:20 -0300

    Controle de fluxo aula 2

--- Alura/CSharp/AprendendoCSharp/10-CalculaPoupanca/Program.cs
+++ Alura/CSharp/AprendendoCSharp/10-CalculaPoupanca/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+        double investimento = 1000;
+
+        // Endimento 0,5% (0,005)mês
+
+        // Mês 1
+        //investimento = investimento + investimento * 0.005;
+        // Mês 2
+        //investimento = investimento + investimento * 0.005;
+        // Mês 3
+        //investimento = investimento + investimento * 0.005;
+
+        Console.WriteLine(investimento);
+
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine($"No mês {mes} você tem {investimento}");
+            mes = mes + 1;
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
--- Alura/CSharp/AprendendoCSharp/10-CalculaPoupanca/obj/Debug/net8.0/P10-CalculaPoupanca.AssemblyInfo.cs
+++ Alura/CSharp/AprendendoCSharp/10-CalculaPoupanca/obj/Debug/net8.0/P10-CalculaPoupanca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P10-CalculaPoupanca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67bbb79a6d11c48919dda1442dceac3e8a4b1e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04868780207a6c07a2d9402d79e5980c5e80b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("P10-CalculaPoupanca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P10-CalculaPoupanca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
