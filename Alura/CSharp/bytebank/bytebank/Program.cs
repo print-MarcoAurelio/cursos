58a65662d19119ee2d57dcd33ca927dc56842174
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-08-06 20:17:07 -0300

    .

---
+++ Alura/CSharp/bytebank/bytebank/Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank;
+
+ContaCorrente contaDoMarco = new ContaCorrente();
+contaDoMarco.titular = "Marco Aurélio";
+contaDoMarco.numero_agencia = 15;
+contaDoMarco.conta = "1010-X";
+contaDoMarco.saldo = 300;
+
+Console.WriteLine($"Saldo da conta do Marco = {contaDoMarco.saldo}");
