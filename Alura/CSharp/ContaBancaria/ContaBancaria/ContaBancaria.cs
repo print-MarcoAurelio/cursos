6022f75aa70e9f40dbeaaf8fcf0e74e86a2236eb
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-08-12 21:54:31 -0300

    .

---
+++ Alura/CSharp/ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,8 @@
+﻿class Conta
+{
+    public string titular;
+    public int id;
+    public float saldo;
+    public int senha;
+}
+
---
+++ Alura/CSharp/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,14 @@
+﻿Conta conta = new Conta();
+conta.titular = "Gui Lima";
+conta.id = 1;
+conta.saldo = 20.00;
+conta.senha = 1234;
+
+Console.WriteLine("INFORMAÇÕES DA CONTA:");
+Console.WriteLine($"Titular {conta.titular}");
+Console.WriteLine($"Saldo atual: {conta.saldo}");
+
+public void exibirInformacoes()
+{
+    Console.WriteLine("INFO")
+}
